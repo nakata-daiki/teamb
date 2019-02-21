145562a06f9f0278e035891fc569fc0f1ffaad12
nakata <nakata.daiki@hfrontier.co.jp>
2019-02-21 20:27:01 +0900

    最初のコミット

---
+++ src/main/java/com/hfrontier/teamb/TeambApplication.java
@@ -0,0 +1,14 @@
+package com.hfrontier.teamb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeambApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeambApplication.class, args);
+	}
+
+}
+
