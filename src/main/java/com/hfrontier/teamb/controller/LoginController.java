3a4fc6d1a4dd856961e67a2d7281efb921d76c3a
nakata-daiki <nakata.daiki@hfrontier.co.jp>
2019-03-07 21:52:12 +0900

    コミット

--- src/main/java/com/hfrontier/teamb/controller/LoginController.java
+++ src/main/java/com/hfrontier/teamb/controller/LoginController.java
@@ -19,11 +19,29 @@ public class LoginController {
 
 	@Transactional(propagation = Propagation.REQUIRED)
 	@RequestMapping(value = {"Regist/Login"}, method = RequestMethod.GET)
+	/**
+	 * ログイン画面
+	 *
+	 * @param loginModel
+	 * @param result
+	 * @param model
+	 * @param request
+	 * @param response
+	 * @return
+	 */
 	public ModelAndView login(@ModelAttribute("LoginModel") LoginModel loginModel,
 			BindingResult result,
 			ModelAndView model,
 			HttpServletRequest request,
 			HttpServletResponse response) {
+
+		// ログイン状態チェック
+
+		// エラーメッセージがあるならモデルにセット
+
+		// セッションにログイン情報があるならモデルにセット
+
+		// 画面表示処理
 
 
 		return model;
@@ -31,13 +49,55 @@ public class LoginController {
 
 	@Transactional(propagation = Propagation.REQUIRED)
 	@RequestMapping(value = {"Regist/Login"}, method = RequestMethod.POST)
+	/**
+	 * ログインボタン押下
+	 *
+	 * @param loginModel
+	 * @param result
+	 * @param model
+	 * @param request
+	 * @param response
+	 * @return
+	 */
 	public ModelAndView postLogin(@ModelAttribute("LoginModel") LoginModel loginModel,
 			BindingResult result,
 			ModelAndView model,
 			HttpServletRequest request,
 			HttpServletResponse response) {
 
+		// 入力内容のチェック（ID、パスワード）
+
+		// ログイン処理
+
+		// 画面遷移処理
+
 		return model;
 	}
 
+	@Transactional(propagation = Propagation.REQUIRED)
+	@RequestMapping(value = {"Regist/AddUser"}, method = RequestMethod.POST)
+	/**
+	 * レジストボタン押下
+	 *
+	 * @param loginModel
+	 * @param result
+	 * @param model
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	public ModelAndView postRegist(@ModelAttribute("LoginModel") LoginModel loginModel,
+			BindingResult result,
+			ModelAndView model,
+			HttpServletRequest request,
+			HttpServletResponse response) {
+
+		// 入力内容チェック（ID、パスワード）
+
+		// 登録処理
+
+		// 画面遷移処理（リダイレクト）
+
+		return model;
+	}
 }
