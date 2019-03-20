4dd5be9a6dca3e537cb957b990f6059e2d213384
arai.kahori <arai.kahori@hfrontier.co.jp>
2019-03-20 20:15:44 +0900

    一旦コメントアウト

--- src/main/java/com/hfrontier/teamb/controller/CommentListController.java
+++ src/main/java/com/hfrontier/teamb/controller/CommentListController.java
@@ -1,8 +1,5 @@
 package com.hfrontier.teamb.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -15,9 +12,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.hfrontier.teamb.common.constant.Constant;
-import com.hfrontier.teamb.repository.Comments;
-import com.hfrontier.teamb.ui.CommentListModel;
-import com.hfrontier.teamb.ui.CommentListModel.CommentsData;
 
 @Controller
 public class CommentListController {
@@ -32,24 +26,24 @@ public class CommentListController {
 			HttpServletRequest request,
 			HttpServletResponse response) {
 
-		CommentListModel commentListModel = new CommentListModel();
-
-		//すべてのコメント？
-		List<Comments> al = new ArrayList<>();
-
-		//
-		List<CommentsData> list = new ArrayList<>();
-
-		//すべてのコメントを一つ一つ確認
-		for (Comments coment : al) {
-			//ユーザー本人のコメントかどうかチェックするメソッド？？？？
-			CommentsData cd = new CommentsData();
-			cd.setId(coment.getId());
-			cd.setComment(coment.getComment());
-			cd.setMyComment(coment.getUserId());
-		}
-
-		model.addObject("comentlist", list);
+//		CommentListModel commentListModel = new CommentListModel();
+//
+//		//すべてのコメント？
+//		List<Comments> al = new ArrayList<>();
+//
+//		//
+//		List<CommentsData> list = new ArrayList<>();
+//
+//		//すべてのコメントを一つ一つ確認
+//		for (Comments coment : al) {
+//			//ユーザー本人のコメントかどうかチェックするメソッド？？？？
+//			CommentsData cd = new CommentsData();
+//			cd.setId(coment.getId());
+//			cd.setComment(coment.getComment());
+//			cd.setMyComment(coment.getUserId());
+//		}
+//
+//		model.addObject("comentlist", list);
 
 		model.setViewName("HTML/board");
 		return model;
